45396789030c5a704596517763642d31c5c1fbe9
Michael Tsang <themichaeltsang@gmail.com>
2013-02-01 20:57:20 -0800

    Add a StaticPages controller

--- ClassNotes/config/routes.rb
+++ ClassNotes/config/routes.rb
@@ -1,4 +1,8 @@
 ClassNotes::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   post "search" => "spash#search", :as => "search"
   root :to => 'splash#index'
 end
