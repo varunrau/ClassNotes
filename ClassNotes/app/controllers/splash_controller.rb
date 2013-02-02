ea4751153177cd814bdd75f822cfc0b0219b4c80
varunrau <varunrau@gmail.com>
2013-02-01 21:37:26 -0800

    added helper modules

--- ClassNotes/app/controllers/splash_controller.rb
+++ ClassNotes/app/controllers/splash_controller.rb
@@ -1,9 +1,11 @@
 class SplashController < ApplicationController
 
   def index
+
   end
 
   def search
+    @params = params
     @lectures, @info, @url, @course, @semester = live_data(params)
   end
 
