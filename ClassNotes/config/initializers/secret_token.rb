065087d719db0d5540da95b2dc2c8e76752eada1
varunrau <varunrau@gmail.com>
2013-02-01 19:48:47 -0800

    added bootstrap

---
+++ ClassNotes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassNotes::Application.config.secret_token = '3bbbeb0f7bbc63a7deae86671653259ec42a9a041ac3019f20ae8e48904cf540f35d33776704b9686307638003c8399b0d1b0482e106d9b1e7472d0182160d8d'
