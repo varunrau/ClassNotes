178be74f9efad9f18fa3fec76a4373c761171f83
varunrau <varunrau@gmail.com>
2013-02-01 23:12:32 -0800

    added function to helper

--- ClassNotes/app/controllers/splash_controller.rb
+++ ClassNotes/app/controllers/splash_controller.rb
@@ -4,7 +4,8 @@ class SplashController < ApplicationController
   end
 
   def search
-    @lectures, @info, @url, @course, @semester = live_data(params)
+    lectures, info, url, course, semester = live_data(params)
+    meeting_times = meetingTimesForLectures(lectures, info)
   end
 
 end
