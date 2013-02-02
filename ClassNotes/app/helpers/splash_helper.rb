5167b42a44911a7e1260edd34726f4fd1448d913
Ajay Shanker Tripathi <mahatmaajaytripathi@gmail.com>
2013-02-02 05:13:18 -0800

    debugged meetingTimesForLectures

--- ClassNotes/app/helpers/splash_helper.rb
+++ ClassNotes/app/helpers/splash_helper.rb
@@ -12,40 +12,41 @@ module SplashHelper
     class_times = []
     times = []
     curr_class = ""
-    classes.each do |lec|
-      lec.each do |text|
-        text.clone
-        if(text =~ / P /)
-          class_name = (text.clone).gsub!(/ [PS] .+/, "")
-          temp = class_name.split(" ")
-          temp.map! { |word| (word =~ /\d/) ? word : word.capitalize }
-          class_name = temp.join(" ")
-          day_string = classes_info[text][:time]
-          day = [
-              (day_string =~ /M/) ? true : false,
-              (day_string =~ /Tu/) ? true : false,
-              (day_string =~ /W/) ? true : false,
-              (day_string =~ /Th/) ? true : false,
-              (day_string =~ /F/) ? true : false
-          ]
-          prof_string = classes_info[text][:instructor]
-          prof = prof_string
-          time_string = classes_info[text][:time]
-          time = time_string.split(" ").last
-          if(class_name != curr_class)
-            class_names = [class_names,class_name]
-            curr_class = class_name
-            class_days = [class_days,days]
-            days = []
-            class_profs = [class_profs,profs]
-            profs = []
-            class_times = [class_times,times]
-            times = []
-
+    classes.each do |l1|
+      l1.each do |l2|
+        l2.each do |text|
+          text.clone
+          if(text =~ / P /)
+            class_name = (text.clone).gsub!(/ [PS] .+/, "")
+            temp = class_name.split(" ")
+            temp.map! { |word| (word =~ /\d/) ? word : word.capitalize }
+            class_name = temp.join(" ")
+            day_string = classes_info[text][:time]
+            day = [
+                (day_string =~ /M/) ? true : false,
+                (day_string =~ /Tu/) ? true : false,
+                (day_string =~ /W/) ? true : false,
+                (day_string =~ /Th/) ? true : false,
+                (day_string =~ /F/) ? true : false
+            ]
+            prof_string = classes_info[text][:instructor]
+            prof = prof_string
+            time_string = classes_info[text][:time]
+            time = time_string.split(" ").last
+            if(class_name != curr_class)
+              class_names = [class_names,class_name]
+              curr_class = class_name
+              class_days = [class_days,days]
+              days = []
+              class_profs = [class_profs,profs]
+              profs = []
+              class_times = [class_times,times]
+              times = []
+            end
+            days = [days,day]
+            profs = [profs,prof]
+            times = [times,time]
           end
-          days = [days,day]
-          profs = [profs,prof]
-          times = [times,time]
         end
       end
     end
