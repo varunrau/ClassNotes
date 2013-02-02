70ae993d8ed0f46a70113d46294eeef3455a74da
varunrau <varunrau@gmail.com>
2013-02-02 12:39:43 -0800

    done

---
+++ ClassNotes/app/assets/javascripts/splash.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+    $('tr').click(function() {
+        document.location.href='https://docs.google.com/document/d/1p3j9ONXpfUiQvy6mJw8ktZ9KLSKlMVGSq5V5qKTNU0A/edit';
+    });
+
+});
