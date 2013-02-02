4ecf7cdcfd5f261414ae6451f1365c60789d7504
varunrau <varunrau@gmail.com>
2013-02-02 05:29:30 -0800

    added redirect

--- ClassNotes/app/controllers/splash_controller.rb
+++ ClassNotes/app/controllers/splash_controller.rb
@@ -7,6 +7,10 @@ class SplashController < ApplicationController
     @lectures, @info, @url, @course, @semester = live_data(params)
     # This will return an array of booleans for class days for each lecture
     @classes, @days, @profs, @times = meetingTimesForLectures(@lectures, @info)
+
+    if @classes.length == 1
+      redirect_to document
+    end
   end
 
 end
