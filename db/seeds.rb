904c5d96205e37ba80a247f8cd55a3090adbbf6e
varunrau <varunrau@gmail.com>
2013-02-04 14:08:31 -0800

    made a complete seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -336,16 +336,16 @@ end
 
 def create_classes
   maps = [
-    # {:days => "M", :semester => "SP"},
-    # {:days => "Tu", :semester => "SP"},
-    # {:days => "W", :semester => "SP"},
-    # {:days => "Th", :semester => "SP"},
-    # {:days => "F", :semester => "SP"},
-    # {:days => "MW", :semester => "SP"}
-    # {:days => "WF", :semester => "SP"},
-    # {:days => "MF", :semester => "SP"},
-    # {:days => "TuTh", :semester => "SP"},
-    # {:days => "MWF", :semester => "SP"},
+    {:days => "M", :semester => "SP"},
+    {:days => "Tu", :semester => "SP"},
+    {:days => "W", :semester => "SP"},
+    {:days => "Th", :semester => "SP"},
+    {:days => "F", :semester => "SP"},
+    {:days => "MW", :semester => "SP"}
+    {:days => "WF", :semester => "SP"},
+    {:days => "MF", :semester => "SP"},
+    {:days => "TuTh", :semester => "SP"},
+    {:days => "MWF", :semester => "SP"}
     {:days => "MTuThF", :semester => "SP"}
   ]
   maps.each { |map|
